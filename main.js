922dbc69e284764cf026a59ff9ffc422c727259a
Ziitteo <ziitteo@gamil.com>
2024-07-09 19:00:13 +0900

    api 주소 변경

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const API_KEY = `0b4d6097f98643fd96713fd37337e51a`
 
 const getLatesNews = async () => {
-  const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
+  const url = new URL(
+    `https://ziitteo-times.netlify.app/top-headlines`
+  );
   
   const response = await fetch(url); 
   const data = response.json();
